b4384d8390d02de05fe6ce5cbd5109ddba78caca
Shweedes <metior22848@mail.ru>
2025-12-21 23:46:52 +0300

    add loading function character data

---
+++ src/services/MarvelService.js
@@ -0,0 +1,37 @@
+class MarvelService {
+    _apiBase = 'https://marvel-server-zeta.vercel.app/'
+
+    _apiKey = 'apikey=d4eecb0c66dedbfae4eab45d312fc1df'
+
+    getResource = async (url) => {
+        let res = await fetch(url)
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`)
+        }
+
+        return await res.json();
+    }
+
+    getAllCharacters = async () => {
+        const response = await this.getResource(`${this._apiBase}characters?limit=9&${this._apiKey}`)
+        return response.data.results.map(character => this._transformCharacter(character) )
+    }
+
+    getCharacter = async (id) => {
+        const response = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`)
+        return this._transformCharacter(response.data.results[0])
+    }
+
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description,
+            thumbnail: `${char.thumbnail.path}.${char.thumbnail.extension}`,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url,
+        }
+    }
+}
+
+export default MarvelService;
