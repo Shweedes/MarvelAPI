605674eadb7cfeeddddf9d02330fda41804cad7e
Shweedes <metior22848@mail.ru>
2025-12-24 22:06:41 +0300

    add ErrorBoundary

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -4,6 +4,7 @@ import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
+import ErrorBoundary from "../errorBoundary/ErrorBoundary.jsx";
 
 import decoration from '../../resources/img/vision.png';
 
@@ -22,10 +23,16 @@ class App extends Component {
             <div className="app">
                 <AppHeader/>
                 <main>
-                    <RandomChar/>
+                    <ErrorBoundary>
+                        <RandomChar/>
+                    </ErrorBoundary>
                     <div className="char__content">
-                        <CharList onCharacterSelectedChange = {this.onCharacterSelectedChange}/>
-                        <CharInfo charId={this.state.selectedChar}/>
+                        <ErrorBoundary>
+                            <CharList onCharacterSelectedChange = {this.onCharacterSelectedChange}/>
+                        </ErrorBoundary>
+                        <ErrorBoundary>
+                            <CharInfo charId={this.state.selectedChar}/>
+                        </ErrorBoundary>
                     </div>
                     <img className="bg-decoration" src={decoration} alt="vision"/>
                 </main>
